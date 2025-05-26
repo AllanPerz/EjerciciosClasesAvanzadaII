2909ece425792c482840cee9531692bf06d57dbd
AllanPerz <aeperez2@uth.hn>
2025-05-25 22:31:30 -0600

    Ejercicios con Clases Avanzada II

--- EjerciciosClase.py
+++ EjerciciosClase.py
@@ -4,7 +4,7 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 class EjerciciosClase:
-    def __init__(self,nombre, perro, gato, a, b Operacion, Total, Descuento, ISV, ValorCompra, Subtotal, TotalPagar, AnioActual, AnioNacimiento, Edad, TablaMultiplicar, Num, Diametro, Suma, Resta, Multiplicacion, Division, x, y, Potencia):
+     def __init__(self, nombre, perro, gato, a, b, operacion, Total, Descuento, ISV, ValorCompra, Subtotal, TotalPagar, AnioActual, AnioNacimiento, Edad, TablaMultiplicar, Num, Diametro, Suma, Resta, Multiplicacion, Division, x, y, Potencia):
         self.nombre = nombre
         self.perro = perro
         self.gato = gato 
@@ -20,7 +20,7 @@ class EjerciciosClase:
         self.AnioCtual = AnioActual
         self.AnioNacimiento = AnioNacimiento
         self.Edad = Edad
-        self.TablaMultiplicar = TablaMultiplicarMultiplicar
+        self.TablaMultiplicar = TablaMultiplicar
         self.Num =Num
         self.Diametro = Diametro
         self.Suma = Suma
@@ -74,5 +74,70 @@ class EjerciciosClase:
             print(f"Tabla de multiplicar del {self.TablaMultiplicar}")
             for i in range(1, 11):
                 print(f"{self.TablaMultiplicar} x {i} = {self.TablaMultiplicar * i}")
+
+        def Ejercicio4(self):
+            self.i = 1
+            self.Num = int(input("Por favor ingrese un numero de la tabla de multiplicar que desee ver: "))
+            print(f"Tabla de multiplicar del {self.Num}")
+            while self.i <= 10:
+                print(f"{self.Num} x {self.i} = {self.Num * self.i}")
+                self.i += 1
+        
+        def EjercicioCicloFor(Self):
+            for i in range(1, 11):
+                print(i)
+
+        def EjercicioTablaDel5(self):
+            print("Tabla del 5")
+            for i in range(1, 11):
+                print(f"5 x {i} = {5 * i}")
+
+        def EjercicioCicloWhile(self):
+            self.i = 1
+            while self.i <= 10:
+                print(self.i)
+                self.i += 1
+        
+        def HolaMundo(self):
+            print("Hola Mundo")
+        
+        def OperacionesAritmeticas(self):
+            a = 10
+            b = 5
+
+            Suma = a + b
+            print(f"{a} + {b} = {Suma}")
+
+            Resta = a - b
+            print(f"{a} - {b} = {Resta}")
+
+            Multiplicacion = a * b
+            print(f"{a} * {b} = {Multiplicacion}")
+
+            Div = a / b
+            print(f"{a} / {b} = {Div}")
+
+            x = int(input("x: "))
+            y = int(input("y: "))
+
+            # Potencia
+            Potencia = x ** y
+            print(f"{x} ^ {y} = {Potencia}")
+
+class Animal:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def comer(self):
+        print(f"{self.nombre} esta comiendo")
+
+    def correr(self):
+        print(f"{self.nombre} esta corriendo")
+
+class Perro(Animal):
+    def ladrar(self):
+        print(f"{self.nombre} gua gua gua")
+
+
 
         
